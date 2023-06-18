46e896dc701fd3d267b85839df33a3bdde28c707
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2023-06-18 19:16:53 +0800

    Added more packages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 const express = require("express");
+const formidableMiddleware = require("express-formidable");
+
+const { DynamoDB, S3 } = require("aws-sdk");
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+const dynamoDB = new DynamoDB.DocumentClient();
+const s3 = new S3();
+
+console.log(dynamoDB, s3);
+
+app.use(formidableMiddleware());
+
+app.put("/", (req, res) => {
+  console.log(req.files);
   res.json({
-    code:1,
-    body:"test"
-  })
+    code: 1,
+    body: "test",
+  });
 });
 
 app.listen(port, () => {
